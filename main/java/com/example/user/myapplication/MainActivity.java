29d6dac7f433a46f65c98f871b90003f47adc91e
Rajkhandor <khandorraj@gmail.com>
2017-05-18 17:34:55 +0530

    Update MainActivity.java

--- main/java/com/example/user/myapplication/MainActivity.java
+++ main/java/com/example/user/myapplication/MainActivity.java
@@ -2,23 +2,41 @@ package com.example.user.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Button add, sub, mul, div, b0, b1, b2, b3, b4, b5, b6, b7, b8, b9, bDec, equalTo, tenPow, ans, del, ac;
     EditText display;
+    TextView ansDisplay;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    //  setContentView(R.layout.activity_main);
+
+        //-------------------------------------------------------
+
+        //Remove title bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+//Remove notification bar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+//set content view AFTER ABOVE sequence (to avoid crash)
         setContentView(R.layout.activity_main);
+
+        //--------------------------------------------------------
+
         add = (Button) findViewById(R.id.plus);
         sub = (Button) findViewById(R.id.minus);
         mul = (Button) findViewById(R.id.multiply);
         div = (Button) findViewById(R.id.divide);
-        display = (EditText) findViewById(R.id.display);
         b0 = (Button) findViewById(R.id.b0);
         b1 = (Button) findViewById(R.id.b1);
         b2 = (Button) findViewById(R.id.b2);
@@ -35,23 +53,11 @@ public class MainActivity extends AppCompatActivity {
         ans = (Button) findViewById(R.id.ans);
         del = (Button) findViewById(R.id.delete);
         ac = (Button) findViewById(R.id.AC);
-        add.setOnClickListener(
-                new View.OnClickListener() {
-                    public void onClick(View v) {
-                        String s = display.getText().toString();
-                        display.setText(s + "+");
-                    }
-                }
-        );
-        sub.setOnClickListener(
-                new View.OnClickListener() {
-                    public void onClick(View v) {
-                        String s = display.getText().toString();
-                        display.setText(s + "-");
 
-                    }
-                }
-        );
+        display = (EditText) findViewById(R.id.display);
+
+        ansDisplay = (TextView) findViewById(R.id.ansDisplay);
+
         b0.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
@@ -151,15 +157,129 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
         );
+        add.setOnClickListener(
+                new View.OnClickListener() {
+                    public void onClick(View v) {
+                        String s = display.getText().toString();
+                        String finalString="";
+                        char check=s.charAt(s.length()-1);
+                        if(check=='+' || check=='-' || check=='X' || check=='/')
+                        {
+                            for(int i=0;i<s.length()-1;i++)
+                                finalString+=s.charAt(i);
+                            finalString+="+";
+                        }
+                        else
+                            finalString=s+"+";
+                        display.setText(finalString);
+                    }
+                }
+        );
+        sub.setOnClickListener(
+                new View.OnClickListener() {
+                    public void onClick(View v) {
+                        String s = display.getText().toString();
+                        String finalString="";
+                        char check=s.charAt(s.length()-1);
+                        if(check=='+' || check=='-' || check=='X' || check=='/')
+                        {
+                            for(int i=0;i<s.length()-1;i++)
+                                finalString+=s.charAt(i);
+                            finalString+="-";
+                        }
+                        else
+                            finalString=s+"-";
+                        display.setText(finalString);
+
+                    }
+                }
+        );
+        mul.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        String s = display.getText().toString();
+                        String finalString="";
+                        char check=s.charAt(s.length()-1);
+                        if(check=='+' || check=='-' || check=='X' || check=='/')
+                        {
+                            for(int i=0;i<s.length()-1;i++)
+                                finalString+=s.charAt(i);
+                            finalString+="X";
+                        }
+                        else
+                            finalString=s+"X";
+                        display.setText(finalString);
+                    }
+                }
+        );
+        div.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        String s =display.getText().toString();
+                        String finalString="";
+                        char check=s.charAt(s.length()-1);
+                        if(check=='+' || check=='-' || check=='X' || check=='/')
+                        {
+                            for(int i=0;i<s.length()-1;i++)
+                                finalString+=s.charAt(i);
+                            finalString+="/";
+                        }
+                        else
+                            finalString=s+"/";
+                        display.setText(finalString);
+                    }
+                }
+        );
         equalTo.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        String a = display.getText().toString();
-                        int ans1 = Integer.parseInt(a);
-                        display.setText(ans1);
+                        String s = display.getText().toString();
+
+                        ansDisplay.setText(s);
+                    }
+                }
+        );
+        del.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        String a=display.getText().toString();
+                        String finalString="";
+                        int len= a.length();
+                        for(int i=0;i<len-1;i++)
+                            finalString=finalString+a.charAt(i);
+                        display.setText(finalString);
+                    }
+                }
+        );
+        ac.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        display.setText("");
+                    }
+                }
+        );
+        tenPow.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        // ** NOT COMPLETED **
+                        String input=display.getText().toString();
+                        input+="X10";
+                        int check=input.length();
+                        while(check==input.length())
+                        {
+                            //do nothing
+                        }
+                        input=display.getText().toString();
+                        display.setText(Html.fromHtml(input+"<sup><small>2</small></sup>"));
                     }
                 }
         );
     }
 }
+
